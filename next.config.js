bb90c6c6a2ea46a50fad84c11390066a26902294
Bartosz <bartek.wiacek1@gmail.com>
2023-11-07 21:18:59 +0100

    fix config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    images: {
+        domains: ['ddragon.leagueoflegends.com'],
+    },
 }
 
 module.exports = nextConfig
