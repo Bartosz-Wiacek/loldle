e084a4e0e22e6c6ea0e9205fe2e9607356debcc0
BWI <BWI@impact.dk>
2023-11-09 15:21:21 +0100

    add media support

--- src/components/TextBlock.tsx
+++ src/components/TextBlock.tsx
@@ -12,9 +12,23 @@ type TextBlockProps = {
 export function TextBlock({header, text, showButton, width, children}: TextBlockProps){
     const [show, setShow] = useState(true);
 
+    function isMedia(): boolean {
+        if (typeof window !== 'undefined') {
+            return window.innerWidth <= 768;
+        }
+        return false;
+    }
+
+    function setWidth(isMedia: boolean) {
+        if (isMedia) {
+            return "80%";
+        }
+        return width?.toString();
+    }
+
     return (
         <>
-            {show ? <div className="text-block-container" style={{width: width}}>
+            {show ? <div className="text-block-container" style={{width: setWidth(isMedia())}}>
                 {(showButton && <button onClick={() => setShow(false)} className={"exitButton"}>X</button>)}
                 <div className="header-block">
                     <h1>{header}</h1>
