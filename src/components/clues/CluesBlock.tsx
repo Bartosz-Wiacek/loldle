bffff18ae2a6e8805c29f78d181ecd9b25368150
BWI <BWI@impact.dk>
2023-11-30 15:01:00 +0100

    add saling image for a splash clue

--- src/components/clues/CluesBlock.tsx
+++ src/components/clues/CluesBlock.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import rawQuotesData from '../../../quotes-data.json';
 import './cluesBlock.css';
 import {TextBlock} from "@/components/TextBlock";
@@ -16,6 +16,12 @@ export function CluesBlock({actualChampionName, header, guessesCounter}: {actual
 
     const [showQuote, setShowQuote] = useState(false);
     const [showSplash, setShowSplash] = useState(false);
+    const [scale, setScale] = useState(4.8);
+
+    const transformOriginProps = useMemo(() => {
+        const allTransformOriginProps = ["top left", "top center", "top right", "center left", "center center", "center right", "bottom left", "bottom center", "bottom right"];
+        return allTransformOriginProps[Math.floor(Math.random() * allTransformOriginProps.length)];
+    }, []);
 
     const randomQuote = useMemo(() => {
         const quotes = quotesData[actualChampionName];
@@ -30,6 +36,13 @@ export function CluesBlock({actualChampionName, header, guessesCounter}: {actual
     const randomInteger = useMemo(() => {
         return Math.floor(Math.random() * 3);
     }, []);
+
+    useEffect(() => {
+        if (scale > 1) {
+            setScale(scale - 0.2);
+        }
+    }, [guessesCounter]);
+
 
     return (
         <>
@@ -63,10 +76,10 @@ export function CluesBlock({actualChampionName, header, guessesCounter}: {actual
                     />
                 </div>
                 {showQuote ? <p className={"clues-text"}>{'"' + randomQuote + '"'}</p> : null}
-                {showSplash ?<div style={{display: "flex", justifyContent: "center"}}>
+                {showSplash ?<div style={{display: "flex", justifyContent: "center", overflowY: "hidden", borderRadius: "5px", marginTop: "5px"}}>
                     <Image
                     src={`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${specialChampionNewUrl(actualChampionName)}_${randomInteger}.jpg`}
-                    alt={"splash-art"} width={500} height={350} />
+                    alt={"splash-art"} width={500} height={350} style={{transform: `scale(${scale})`, transformOrigin: transformOriginProps}} />
                 </div>  : null}
             </TextBlock>
         </>
