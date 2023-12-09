f67b4732fe0ebe5cec0e2198b1b5e9397fdb4f06
Bartosz <bartek.wiacek1@gmail.com>
2023-12-09 12:51:37 +0100

    exit button fix

--- src/components/clues/Modal.tsx
+++ src/components/clues/Modal.tsx
@@ -4,16 +4,16 @@ import ModalContent from "@/components/clues/ModalContent";
 
 export default function Modal({ modal, toggleModal, cssStyle }: { modal: boolean, toggleModal: () => void, cssStyle?: React.CSSProperties }) {
 
-    const closeOnOverlayClick = (e: any) => {
-        if (e.target.className === 'overlay') {
-            toggleModal();
-        }
-    }
+    // const closeOnOverlayClick = (e: any) => {
+    //     if (e.target.className === 'overlay') {
+    //         toggleModal();
+    //     }
+    // }
 
     return (
         <>
             {modal ?
-            <div className={"modal"} onClick={closeOnOverlayClick}>
+            <div className={"modal"} >
                 <div className={"overlay"}>
                     <TextBlock header={"How to play?"} showButton={true} onClick={toggleModal} cssStyle={cssStyle}>
                         <ModalContent />
